c4075646205fe7b095b069ac3ec82bb1ec865171
KevinTran3011 <tuankiettran30112000@gmail.com>
2023-07-25 03:28:06 +1000

    Added the navigation bar

--- src/App.js
+++ src/App.js
@@ -1,41 +1,21 @@
-// import './categories.style.scss';
-import './component/category-item/category-item.component';
-import Directory from './component/directory/directory.component'
+import {Routes, Route, Outlet} from 'react-router-dom';
+
+import Home from "./routes/home/home.component";
+import Navigation from './routes/navigation/navigation.component'
+
+
 
 const App = () => {
-  // directional array so that code won't get repeated
-  const categories = [
-    {
-      "id": 1,
-      "title": "Hats",
-      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-    },
-    {
-      "id": 2,
-      "title": "Jackets",
-      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-    },
-    {
-      "id": 3,
-      "title": "Sneakers",
-      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-    },
-    {
-      "id": 4,
-      "title": "Women",
-      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-    },
-    {
-      "id": 5,
-      "title": "Mens",
-      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-    }
-  ];
+      return(
+        <Routes>
+          <Route path = '/' element = {<Navigation/>}>
+          <Route index element = {<Home />}/>
 
-  return (
 
-      <Directory categories = {categories}/>
-  );
-};
+          </Route>
+
+        </Routes>
+      ) ;
+}
 
 export default App;
---
+++ src/routes/home/home.component.jsx
@@ -0,0 +1,45 @@
+
+import { Outlet } from 'react-router-dom';
+
+import Directory from '../../component/directory/directory.component'
+
+const Home = () => {
+  // directional array so that code won't get repeated
+  const categories = [
+    {
+      "id": 1,
+      "title": "Hats",
+      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
+    },
+    {
+      "id": 2,
+      "title": "Jackets",
+      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
+    },
+    {
+      "id": 3,
+      "title": "Sneakers",
+      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
+    },
+    {
+      "id": 4,
+      "title": "Women",
+      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
+    },
+    {
+      "id": 5,
+      "title": "Mens",
+      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
+    }
+  ];
+
+  return (
+        <div>
+          {/* <Outlet/> */}
+          <Directory categories = {categories}/>
+
+        </div>
+  );
+};
+
+export default Home;
