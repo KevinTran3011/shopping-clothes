4f415fbd3faf549c55eb08bd9e17182f94a50e51
KevinTran3011 <tuankiettran30112000@gmail.com>
2023-07-31 00:35:42 +1000

    added the product cards

--- src/index.js
+++ src/index.js
@@ -5,17 +5,25 @@ import { UserProvider } from './component/context/user.context';
 import { BrowserRouter } from 'react-router-dom';
 import 'tachyons';
 import App from './App';
+import { ProductProvider } from './component/context/product.context';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <UserProvider>
-    <App />
+      <UserProvider>
+        <ProductProvider> 
+          {/* Product provider placed inside user provider so that products can access user's information */}
 
-    </UserProvider>
-</BrowserRouter>
+        <App />
+
+
+        </ProductProvider>
+
+
+      </UserProvider>
+  </BrowserRouter>
   </React.StrictMode>
 );
 
