bcbbf1f30825e0b137cd45acc42839252ea94322
KevinTran3011 <tuankiettran30112000@gmail.com>
2023-08-05 10:05:52 +1000

    fixed the buttons

--- src/component/button/button.styles.jsx
+++ src/component/button/button.styles.jsx
@@ -1,68 +1,57 @@
 import styled from 'styled-components';
 
-
 export const BasedButton = styled.button`
+  /* Common styles for all screen sizes */
+  min-width: 165px;
+  width: auto;
+  height: 50px;
+  letter-spacing: 0.5px;
+  line-height: 50px;
+  padding: 0 35px;
+  font-size: 15px;
+  background-color: black;
+  color: white;
+  text-transform: uppercase;
+  font-family: 'Open Sans Condensed';
+  font-weight: bolder;
+  border: none;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
 
-      min-width: 165px;
-      width: auto;
-      height: 50px;
-      letter-spacing: 0.5px;
-      line-height: 50px;
-      padding: 0 35px 0 35px;
-      font-size: 15px;
-      background-color: black;
-      color: white;
-      text-transform: uppercase;
-      font-family: 'Open Sans Condensed';
-      font-weight: bolder;
-      border: none;
-      cursor: pointer;
-      display: flex;
-      justify-content: center;
+  &:hover {
+    background-color: white;
+    color: black;
+    border: 1px solid black;
+  }
 
-      &:hover {
-        background-color: white;
-        color: black;
-        border: 1px solid black;
-      }
+  @media screen and (max-width: 480px) {
+    min-width: 120px;
+    height: 40px;
+    line-height: 40px;
+    padding: 0 20px;
+    font-size: 14px;
+  }
 `;
 
-
-// extends the basebutton with additional styling
 export const GoogleSignInButton = styled(BasedButton)`
+  background-color: #4285f4;
+  color: white;
 
-        background-color: #4285f4;
-        color: white;
-
-        &:hover {
-          background-color: #357ae8;
-          border: none;
-        }
-      
-
+  &:hover {
+    background-color: #357ae8;
+    border: none;
+  }
 `;
 
 export const InvertedButton = styled(BasedButton)`
+  background-color: white;
+  color: black;
+  border: 1px solid black;
 
-      background-color: white;
-      color: black;
-      border: 1px solid black;
-
-      &:hover {
-        background-color: black;
-        color: white;
-        border: none;
-      }
-    
-
+  &:hover {
+    background-color: black;
+    color: white;
+    border: none;
+  }
 `;
-
-
-
-
-
-
-  
-    
-
-  
